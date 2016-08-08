66df07992d8b2695fcd9576212f63c24e678c372
Martin van Es <martin@mrvanes.com>
2016-08-08 21:24:36 +0200

    fb0 vsync callback based updates

--- main.c
+++ main.c
@@ -1,27 +1,37 @@
-
 #include <stdio.h>
 #include <syslog.h>
 #include <fcntl.h>
 #include <linux/fb.h>
 #include <sys/mman.h>
+#include <sys/time.h>
 
 #include <bcm_host.h>
 
-int process() {
-    DISPMANX_DISPLAY_HANDLE_T display;
-    DISPMANX_MODEINFO_T display_info;
-    DISPMANX_RESOURCE_HANDLE_T screen_resource;
-    VC_IMAGE_TRANSFORM_T transform;
-    uint32_t image_prt;
-    VC_RECT_T rect1;
-    int ret;
-    int fbfd = 0;
-    char *fbp = 0;
+DISPMANX_DISPLAY_HANDLE_T display;
+DISPMANX_MODEINFO_T display_info;
+DISPMANX_RESOURCE_HANDLE_T screen_resource;
+VC_IMAGE_TRANSFORM_T transform;
+uint32_t image_prt;
+VC_RECT_T rect1;
+int ret;
+int fbfd = 0;
+char *fbp = 0;
 
-    struct fb_var_screeninfo vinfo;
-    struct fb_fix_screeninfo finfo;
+struct fb_var_screeninfo vinfo;
+struct fb_fix_screeninfo finfo;
 
+unsigned char busy = 0;
 
+void copy_screen(DISPMANX_UPDATE_HANDLE_T handle, void* arg) {
+    if (!busy) {
+      busy = 1;
+      vc_dispmanx_snapshot(display, screen_resource, 0);
+      vc_dispmanx_resource_read_data(screen_resource, &rect1, fbp, vinfo.xres * vinfo.bits_per_pixel / 8);
+      busy = 0;
+    }
+}
+
+int main(int argc, char **argv) {
     bcm_host_init();
 
     display = vc_dispmanx_display_open(0);
@@ -35,7 +45,6 @@ int process() {
         return -1;
     }
     syslog(LOG_INFO, "Primary display is %d x %d", display_info.width, display_info.height);
-
 
     fbfd = open("/dev/fb1", O_RDWR);
     if (fbfd == -1) {
@@ -63,7 +72,7 @@ int process() {
 
     fbp = (char*) mmap(0, finfo.smem_len, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
     if (fbp <= 0) {
-        syslog(LOG_ERR, "Unable to create mamory mapping");
+        syslog(LOG_ERR, "Unable to create memory mapping");
         close(fbfd);
         ret = vc_dispmanx_resource_delete(screen_resource);
         vc_dispmanx_display_close(display);
@@ -72,24 +81,18 @@ int process() {
 
     vc_dispmanx_rect_set(&rect1, 0, 0, vinfo.xres, vinfo.yres);
 
-    while (1) {
-        ret = vc_dispmanx_snapshot(display, screen_resource, 0);
-        vc_dispmanx_resource_read_data(screen_resource, &rect1, fbp, vinfo.xres * vinfo.bits_per_pixel / 8);
-        usleep(25 * 1000);
-    }
+    // Initialise callback
+    vc_dispmanx_vsync_callback(display, (DISPMANX_CALLBACK_FUNC_T)copy_screen, NULL);
 
+    // Do nothing
+    pause();
+
+    // Cleanup on kill signal
     munmap(fbp, finfo.smem_len);
     close(fbfd);
-    ret = vc_dispmanx_resource_delete(screen_resource);
+    vc_dispmanx_resource_delete(screen_resource);
+    vc_dispmanx_vsync_callback(display, NULL, NULL);
     vc_dispmanx_display_close(display);
+
+    return;
 }
-
-int main(int argc, char **argv) {
-    setlogmask(LOG_UPTO(LOG_DEBUG));
-    openlog("fbcp", LOG_NDELAY | LOG_PID, LOG_USER);
-
-    return process();
-}
-
-
-
